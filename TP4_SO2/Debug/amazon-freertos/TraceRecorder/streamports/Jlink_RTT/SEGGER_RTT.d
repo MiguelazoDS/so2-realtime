1033240e8e842753756abef0f117d0d135e815b7
Cazajous Miguel A <migue7490@gmail.com>
2018-11-06 10:09:43 -0300

    hello world snapshot saved

--- TP4_SO2/Debug/amazon-freertos/TraceRecorder/streamports/Jlink_RTT/SEGGER_RTT.d
+++
@@ -1,9 +0,0 @@
-amazon-freertos/TraceRecorder/streamports/Jlink_RTT/SEGGER_RTT.o \
- amazon-freertos/TraceRecorder/streamports/Jlink_RTT/SEGGER_RTT.d: \
- ../amazon-freertos/TraceRecorder/streamports/Jlink_RTT/SEGGER_RTT.c \
- ../amazon-freertos/TraceRecorder/streamports/Jlink_RTT/include/SEGGER_RTT.h \
- ../amazon-freertos/TraceRecorder/streamports/Jlink_RTT/include/SEGGER_RTT_Conf.h
-
-../amazon-freertos/TraceRecorder/streamports/Jlink_RTT/include/SEGGER_RTT.h:
-
-../amazon-freertos/TraceRecorder/streamports/Jlink_RTT/include/SEGGER_RTT_Conf.h:
